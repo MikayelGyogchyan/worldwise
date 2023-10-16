97dce928330613ed2ea79e2e8d52c3653b2600d2
DavitGyogchyan <mikayel1999@mail.com>
2023-10-16 23:06:42 +0000

    CSS Modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom"
+import AppNav from "../components/AppNav"
 import PageNav from "../components/PageNav"
 
 function Homepage() {
   return (
     <div>
       <PageNav/>
+      <AppNav/>
       <h1>
         WorldWise
       </h1>
 
-      <Link to='/pricing'>Pricing</Link>
+      <Link to='/app'>Go to the App</Link>
 
     </div>
   )
