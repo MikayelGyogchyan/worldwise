14191e8aec38156fa1fa607436962a119e25f7e0
Mikayel <gyogchyanmikayel@gmail.com>
2023-10-20 21:28:35 +0000

    Add files via upload
    Adding Fake Authentication: Implementing "Login"

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,23 +1,21 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 const initialState = {
   user: null,
-  isAuthenticated: false
-}  
+  isAuthenticated: false,
+}; 
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'login':
-      return {...state, user: action.payload, isAuthenticated: true}
-  
-    case 'logout':
-      return {...state, user: null, isAuthenticated: false}
-    }
-
-    default: 
-      throw new Error('Unknown action')
+    case "login":
+      return { ...state, user: action.payload, isAuthenticated: true };
+    case "logout":
+      return { ...state, user: null, isAuthenticated: false };
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
 const FAKE_USER = {
@@ -45,6 +43,7 @@ function useAuth() {
   const context = useContext(AuthContext)
   if(context === undefined) 
     throw new Error('AuthContext was used outside AuthProvider')
+  return context
 }
 
 export {AuthProvider, useAuth}
